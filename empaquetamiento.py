3f5cfbbee9016e72ded3a5dd40cc61fad7a2af67
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2023-05-24 21:25:59 -0300

    Refactor con for

--- empaquetamiento.py
+++ empaquetamiento.py
@@ -1,29 +1,26 @@
 def empaquetamiento(T, index, envase_actual, soluciones, indices_usados):
     if sum(envase_actual) > 1:
+        envase_actual.pop()
+        indices_usados.pop()
         return False
     
-    if len(T) == len(indices_usados):
+    if len(T) == len(indices_usados) or sum(envase_actual) == 1:
         return True
     
-    if sum(envase_actual) == 1:
-        soluciones.append(envase_actual[:])
-        return True
-
     for i in range(index, len(T)):
+        if i in indices_usados: continue
         envase_actual.append(T[i])
         indices_usados.append(i)
-        if not empaquetamiento(T, index + 1, envase_actual, soluciones, indices_usados):
-            envase_actual.pop()
-            indices_usados.pop()
+        if empaquetamiento(T, index + 1, envase_actual, soluciones, indices_usados):
+            soluciones.append(envase_actual[:])
+            continue
         elif i == len(T) - 1:
             soluciones.append(envase_actual[:])
-            empaquetamiento(T, 0, [], soluciones, indices_usados)
-    # Empiezo denuevo con otro envase vacio
-    return empaquetamiento(T, 0, [], soluciones, indices_usados)
+    
+    return False
 
 soluciones = []
 #T = [0.4, 0.8, 0.5, 0.1, 0.7, 0.6, 0.1, 0.4, 0.2, 0.2]
-#T = [0.6, 0.5]
-T = [0.2, 0.6]
+T = [0.5, 0.6]
 empaquetamiento(T, 0, [], soluciones, [])
 print(soluciones)
