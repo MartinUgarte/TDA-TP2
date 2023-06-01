65f8b0659501f7fa97771153476dbe6a68da546e
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2023-05-31 23:24:31 -0300

    iluminacion

--- empaquetamiento.py
+++ empaquetamiento.py
@@ -11,16 +11,25 @@ def empaquetamiento(T, index, envase_actual, soluciones, indices_usados):
         if i in indices_usados: continue
         envase_actual.append(T[i])
         indices_usados.append(i)
+        
         if empaquetamiento(T, index + 1, envase_actual, soluciones, indices_usados):
             soluciones.append(envase_actual[:])
-            continue
-        elif i == len(T) - 1:
+            envase_actual = []
+
+        if sum(envase_actual) < 1 and index == len(T) - 1:
             soluciones.append(envase_actual[:])
-    
+            envase_actual = []
+
+        if sum(envase_actual) < 1 and i == len(T) - 1:
+            if envase_actual:
+                envase_actual.pop()
+                indices_usados.pop()
+            empaquetamiento(T, 0, envase_actual, soluciones, indices_usados)
+        
     return False
 
 soluciones = []
 #T = [0.4, 0.8, 0.5, 0.1, 0.7, 0.6, 0.1, 0.4, 0.2, 0.2]
-T = [0.5, 0.6]
+T = [0.5, 0.4, 0.6]
 empaquetamiento(T, 0, [], soluciones, [])
 print(soluciones)
