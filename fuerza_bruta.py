d27a52321f44a30e9ab3084d1683c818231f00ed
Martu <mlozano@fi.uba.ar>
2023-06-02 10:00:31 -0300

    backtracking funciona un poco mejor

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -6,29 +6,42 @@ def con_alguno_suma(nro, lista):
             return True
     return False
 
+
 def empaquetamiento_fb(T):
     start_time = time.time()
     soluciones = []
+    soluciones_parciales = []
     envase_actual = []
+    
+    for i in range(len(T)):
+        soluciones_parciales = []
+        envase_actual = []
+        t_aux = T[i:] + T[:i]
+        while t_aux:
+            envase_actual.append(t_aux.pop(0))
 
-    t_aux = T[:]
-    while t_aux:
-        envase_actual.append(t_aux.pop(0))
+            if sum(envase_actual) > 1:
+                t_aux.append(envase_actual.pop())
+                
+            if sum(envase_actual) == 1:
+                soluciones_parciales.append(envase_actual[:])
+                envase_actual = []
 
-        if sum(envase_actual) > 1:
-            t_aux.append(envase_actual.pop())
+            elif con_alguno_suma(sum(envase_actual), t_aux):
+                continue
+
+            else: 
+                soluciones_parciales.append(envase_actual[:])
+                envase_actual = []
             
-        if sum(envase_actual) == 1:
-            soluciones.append(envase_actual[:])
-            envase_actual = []
+            if len(soluciones_parciales) >= len(soluciones) and len(soluciones) != 0:
+                break
 
-        elif con_alguno_suma(sum(envase_actual), t_aux):
-            continue
+        if len(soluciones_parciales) < len(soluciones) or len(soluciones) == 0:
+            soluciones = soluciones_parciales[:]
 
-        else: 
-            soluciones.append(envase_actual[:])
-            envase_actual = []
     end_time = time.time()
     return soluciones, end_time - start_time
+
 
 
